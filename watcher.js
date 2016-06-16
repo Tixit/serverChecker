cd12898d675d12298a9d80d4946c3cdb4fd3c7a4
fresheneesz <fresheneesz@gmail.com>
2016-06-15 19:07:51 -0700

    various bug fixes and minor improvements

--- watcher.js
+++ watcher.js
@@ -12,12 +12,14 @@ module.exports = proto(function(){
     // args - An object with the following properties:
         // host
         // port
+        // path - The path to ping the server with (default: '/')
         // secure - If true, will use https
         // errorSubject - The subject for the error email
         // name - The name of what's being watched
         // errorRecipients - Info about who to send notifications to. An object where the key is an email to send to and the value is an object with the properties:
             // maxConsecutive - The number of times in a row a notification will happen before it switches to notifying only some checks
             // cooldown - After maxConsecutive, a notification will happen once for every cooldown attempts
+            // minAttempts - The minimum number of attempts before notifying will start for this recipient
         // smtpTransportOptions - the options to give to nodemailer.createTransport
             // assumes that auth.user is defined as the from email
         // onError(e) - A callback that's called whenever a notification couldn't be made
@@ -25,6 +27,7 @@ module.exports = proto(function(){
         this.unreachableCount = 0
         this.host = args.host
         this.port = args.port
+        this.path = args.path || '/'
         this.errorSubject = args.errorSubject
         this.name = args.name
         this.errorRecipients = args.errorRecipients
@@ -32,6 +35,8 @@ module.exports = proto(function(){
         this.maxConsecutiveErrorNotifications = args.maxConsecutiveErrorNotifications
         this.backoffNotificationMod = args.backoffNotificationMod
         this.onError = args.onError
+
+        console.log(args.path)
 
         if(args.secure) {
             this.protocol = https
@@ -45,24 +50,35 @@ module.exports = proto(function(){
             serverReq(this.protocol, {
                 host: this.host,
                 port: this.port,
-                path: '/',
-                method: 'GET'
+                path: this.path,
+                method: 'GET', rejectUnauthorized:false
             }).wait()
             this.unreachableCount = 0
+            console.log(this.name+' ok!')
         } catch(e) {
+            if(e.message.indexOf("ECONNREFUSED") === -1) {
+                console.log(e)
+            }
             this.unreachableCount++
 
             var recipients = []
             for(var email in this.errorRecipients) {
                 var info = this.errorRecipients[email]
-                if(this.unreachableCount < info.maxConsecutive || this.unreachableCount%info.cooldown === 0) {
+
+                //console.log("min: "+info.minAttempts+" count:"+this.unreachableCount+" countAfterMin:"+(this.unreachableCount-info.minAttempts))
+
+                var countAfterMin = (this.unreachableCount-info.minAttempts)
+                if( (info.minAttempts === undefined || this.unreachableCount >= info.minAttempts)
+                  &&(countAfterMin < info.maxConsecutive || countAfterMin>0 && countAfterMin%info.cooldown === 0)
+                ) {
                     recipients.push(email)
                 }
             }
 
+            console.log("notifying about "+this.name+": "+recipients)
             if(recipients.length > 0) {
-                var errorMessage = "Couldn't reach "+this.name+' at '+this.host+":"+this.port
-                notify(this.smtpTransportOptions, recipients, this.errorSubject, errorMessage, e, this.onError)
+//                var errorMessage = "Couldn't reach "+this.name+' at '+this.host+":"+this.port
+//                notify(this.smtpTransportOptions, recipients, this.errorSubject, errorMessage, e, this.onError)
             }
         }
     }
@@ -90,7 +106,7 @@ function serverReq(protocol, reqOptions) {
     })
 
     req.on('error', function(e) {
-        f.throw(e.message)
+        f.throw(e)
     })
 
     req.end()
